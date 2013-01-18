dd9ab72c9c645a6e0b141650a46d831eb8a59418
Damian Krzeminski <pirxpilot@code42day.com>
2013-01-18 10:52:25 -0800

    Initial implementation

---
+++ index.js
@@ -0,0 +1,37 @@
+var dataset = require('dataset');
+
+module.exports = analytics;
+
+/*global document, window */
+
+function loadScript(scriptUrl, async) {
+  var js, fjs;
+
+  js = document.createElement('script');
+  js.src = scriptUrl;
+  if (async) {
+    js.async = true;
+  }
+  fjs = document.getElementsByTagName('script')[0];
+  fjs.parentNode.insertBefore(js, fjs);
+}
+
+function gaScriptUrl() {
+  return ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') +
+    '.google-analytics.com/ga.js';
+}
+
+function analytics(propertyId) {
+  if (!propertyId) {
+    propertyId = dataset(document.body, 'gaPropertyId');
+    if (!propertyId) {
+      return;
+    }
+  }
+
+  var gaq = window._gaq = window._gaq || [];
+  gaq.push(['_setAccount', propertyId]);
+  gaq.push(['_trackPageview']);
+
+  loadScript(gaScriptUrl(), true);
+}
